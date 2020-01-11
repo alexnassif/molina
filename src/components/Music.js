ea7ca6b8f6ec87dd0caca724a66c1cb7a8626d9e
Alex Nassif <alexnassif@Alexs-MBP.home>
2020-01-10 16:23:21 -0800

    added lists

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import YouTube from 'react-youtube';
+import withStyles from "@material-ui/core/styles/withStyles";
+import List from "@material-ui/core/List";
+import ListItem from '@material-ui/core/ListItem';
 
-const Music = () => {
-    return(
+const Music = ({classes}) => {
+    return (
         <div>
-            music
+            <List className={classes.root}>
+                <ListItem className={classes.details}>
+                    <YouTube videoId={"JzhIEXiYto4"}  />
+
+
+                </ListItem>
+                <ListItem className={classes.details}>
+                    <YouTube videoId={"NYl7HNNQ1Wo"}  />
+
+
+                </ListItem>
+                <ListItem className={classes.details}>
+                    <YouTube videoId={"MVoQT3uNg4I"}  />
+
+
+                </ListItem>
+            </List>
         </div>
     );
 };
 
-export default Music;
+const useStyles = {
+  root: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  details: {
+    justifyContent: "center"
+  },
+  link: {
+    color: "#424242",
+    textDecoration: "none",
+    "&:hover": {
+      color: "black"
+    }
+  }
+};
+export default withStyles(useStyles)(Music);
